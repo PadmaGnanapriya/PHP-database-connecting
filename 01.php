49c3d1fc2c84cc04fbf7eeba9951eed8d6879270
SE/2017/014 <gnanapri_se17014@kln.ac.lk>
2020-09-03 13:35:49 +0530

    PHP database programming testing

---
+++ 01.php
@@ -0,0 +1,15 @@
+<?php
+
+    // echo "PHP";
+    $num= array(5,6,7,8,9,10);
+    $number[0]=5;
+    $number[1]=6;
+    $number[2]=7;
+    $number[3]=3;
+    $number[4]=1;
+
+    foreach($number as $x){
+        echo "Value is $x <br>";
+    }
+
+?>
